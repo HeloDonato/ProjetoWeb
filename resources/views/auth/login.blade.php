942786939c0824b5dcd816ed650b0803bfdc2f7d
Heloisa <helodonato7@gmail.com>
2021-11-12 21:17:47 -0300

    Alteração do layout da tela de Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,76 +2,69 @@
 
 @section('content')
 
-<div class="container-fluid fundo">
-    <div class="content">
-        <div class="row">
-            <div class="col-md-6">
-                <div class="fundo-img">
-                    <img src="{{ asset('img/logoif.png') }}" id="img-portarias">
-                </div>
-            </div>
-            <div class="col-md-6 div-formCS">
+<div class="container-fluid">
+    <div class="content-login" style="justify-content: center;">
+        <div class="row-form">
+            <div class="col-md-12 col-form-login">
                 <div class="tit-form">
-                    <h1 class="tit-form-ser">Login</h1>
+                    <h1 class="tit-form-login">Login</h1>
                 </div>
     
-                <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                <form method="POST" action="{{ route('login') }}" class="formulario-login">
+                    @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
+                    <div class="form-group row justify-content-center">
+                        <div class="col-md-5">
+                            <input id="email" type="email" placeholder="Email" autofocus="none" class="form-control cad-login @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" autofocus="none" class="form-control cad-servidor @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ 'Email ou senha incorretos!' }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ 'Email ou senha incorretos!' }}</strong>
-                                    </span>
-                                @enderror
+                    <div class="form-group row justify-content-center">
+                        
+                        <div class="col-md-5">
+                            <input id="password"  placeholder="Senha" type="password" class="form-control cad-login @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="form-group row justify-content-center">
+                        <div class="col-md-5">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                <label class="form-check-label text-dark" for="remember">
+                                    {{ __('Lembrar-me') }}
+                                </label>
                             </div>
                         </div>
+                    </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Senha') }}</label>
+                    <div class="form-group row justify-content-center">
+                        <div class="col-md-5 last-line-form-login">
+                            <button type="submit" class="btn btn-entrar">
+                                {{ __('Entrar') }}
+                            </button>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control cad-servidor @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                            @if (Route::has('password.request'))
+                                <a class="btn btn-entrar" href="{{ route('password.request') }}">
+                                    {{ __('Alterar Senha') }}
+                                </a>
+                            @endif
                         </div>
+                    </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label text-dark" for="remember">
-                                        {{ __('Lembrar-me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-entrar">
-                                    {{ __('Entrar') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="link-card" href="{{ route('password.request') }}">
-                                        {{ __('Alterar Senha') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                </form>
             </div>
         </div>
     </div>
