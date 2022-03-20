06276c832d8a24f562665da153b14594bb71cdf1
liroutw <portugasxd@gmail.com>
2022-03-20 19:35:22 -0300

    Remocao de um atributo complementar

--- database/migrations/2022_03_01_195627_create_cursos_table.php
+++ database/migrations/2022_03_01_195627_create_cursos_table.php
@@ -16,6 +16,7 @@ class CreateCursosTable extends Migration
         Schema::create('cursos', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('descricao');
         });
     }
 
